792bd52007428dfff49b2aec26c9244348c0c96b
HristinaBuyanova <hristina-a@mail.ru>
2023-11-21 21:03:13 +0300

    Add func getData

--- HW19.playground/Contents.swift
+++ HW19.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func getData(urlRequest: String) {
+    let urlRequest = URL(string: urlRequest)
+    guard let url = urlRequest else { return }
+    URLSession.shared.dataTask(with: url) { data, responce, error in
+        if error != nil {
+
+        } else if let responce = responce as? HTTPURLResponse, responce.statusCode == 200 {
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+        }
+    }.resume()
+}
