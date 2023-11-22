e5acc110c13f956267c1e8f852d6b0cbb840acf2
HristinaBuyanova <hristina-a@mail.ru>
2023-11-22 22:47:50 +0300

    Transformation func getData

--- HW19.playground/Contents.swift
+++ HW19.playground/Contents.swift
@@ -2,13 +2,28 @@ import UIKit
 
 func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
-    guard let url = urlRequest else { return }
+    guard let url = urlRequest else {
+        print("Неверный формат ссылки")
+        return }
     URLSession.shared.dataTask(with: url) { data, responce, error in
         if error != nil {
+            print("Error: \(String(describing: error))")
+        } else if let responce = responce as? HTTPURLResponse {
+            switch responce.statusCode {
+            case 200:
+                print("Успешно. Код 200.")
+            default:
+                print("Код ответа: \(responce.statusCode)")
+            }
 
-        } else if let responce = responce as? HTTPURLResponse, responce.statusCode == 200 {
-            guard let data = data else { return }
-            let dataAsString = String(data: data, encoding: .utf8)
+            guard let data = data, let dataAsString = String(data: data, encoding: .utf8) else {
+                print("Ошибка получения данных")
+                return
+            }
+            print("Data: \(dataAsString)")
         }
     }.resume()
 }
+
+
+getData(urlRequest: "http://dog-api.kinduff.com/api/facts?number=5")
